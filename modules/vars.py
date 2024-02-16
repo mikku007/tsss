f6cb11859bbf62fff53554794675d37b7cf6fed7
Lordraaama <152474787+Lordraaama@users.noreply.github.com>
2024-02-16 13:37:56 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = 22609670
-api_hash = "3506d8474ad1f4f5e79b7c52a5c3e88d"
-bot_token = "6593680486:AAFnXOzLpAeUl9T8fkLPeU5POTMigjeZlFs"
+api_id = 26765615
+api_hash = "dc9335059a80b3c8753d26e36d9e28f4"
+bot_token = "6309266727:AAFi5vX5YPH4PWRjvsaw3u6dVl93YW4qM_Q"
